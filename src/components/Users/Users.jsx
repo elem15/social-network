44f2ab99095c5fac9580b156259167b0e1dacef4
unknown <elem15ten@gmail.com>
2021-06-19 13:08:14 +0300

    class component componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,13 +5,12 @@ import userPhoto from '../../../src/assets/images/user.png'
 
 class Users extends React.Component{
 
-    constructor(props) {
-        super(props);
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
                 this.props.setUsers(response.data.items)
-            })
+        })
     }
-    
 
     render() {
         return (
