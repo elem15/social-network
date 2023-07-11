d5c148eb3953587ae710c4c5ba87be2455854726
Elem <elem15@yandex.ru>
2023-07-11 13:33:23 +0300

    fix: add local links to audio

--- src/redux/music-reducer.ts
+++ src/redux/music-reducer.ts
@@ -1,35 +1,35 @@
-import {updateObjectInArray} from "../utils/object-helpers";
-import {ComposersType} from "../Types/Types";
-
-
+import { ComposersType } from "../Types/Types";
+import Bach from "../assets/music/Bach.mp3";
+import Beethoven from "../assets/music/Ludwig Van Beethoven.mp3";
+import Mozart from "../assets/music/Wolfgang Amadeus Mozart - Allegro moderato.mp3";
 
 const SELECT_COMPOSER = 'SELECT_COMPOSER';
 
 let initialState = {
     composers: [
-    {id: 0, name: 'Johann Sebastian Bach', isLike: true, sample: 'https://dl1.mp3party.net/download/8817358'},
-    {id: 1, name: 'Wolfgang Amadeus Mozart', isLike: false, sample: 'https://dl1.mp3party.net/download/8445800'},
-    {id: 2, name: 'Ludwig van Beethoven', isLike: false, sample: 'https://dl1.mp3party.net/download/9067182'}
-] as Array<ComposersType>
+        { id: 0, name: 'Johann Sebastian Bach', isLike: true, sample: Bach },
+        { id: 1, name: 'Wolfgang Amadeus Mozart', isLike: false, sample: Mozart },
+        { id: 2, name: 'Ludwig van Beethoven', isLike: false, sample: Beethoven }
+    ] as Array<ComposersType>
 }
 type InitialStateActionType = typeof initialState
 
-const musicReducer = (state = initialState, action:any): InitialStateActionType => {
+const musicReducer = (state = initialState, action: any): InitialStateActionType => {
 
     switch (action.type) {
         case SELECT_COMPOSER:
             return {
                 ...state, composers:
-                 state.composers.map(u => {
-                    if (u.id === action.id) {
-                        return {...u, isLike: !u.isLike}
-                    } else {
-                        return {
-                            ...u, isLike: false
+                    state.composers.map(u => {
+                        if (u.id === action.id) {
+                            return { ...u, isLike: !u.isLike }
+                        } else {
+                            return {
+                                ...u, isLike: false
+                            }
                         }
-                    }
-                    return u
-                })
+                        return u
+                    })
             };
 
         default:
@@ -37,6 +37,6 @@ const musicReducer = (state = initialState, action:any): InitialStateActionType 
     }
 }
 
-export const selectComposer = (id:number) => ({type: SELECT_COMPOSER, id});
+export const selectComposer = (id: number) => ({ type: SELECT_COMPOSER, id });
 
 export default musicReducer;
